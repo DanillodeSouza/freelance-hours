a945eb5e772a158a70aae7aa59c7874a073ea08a
Danillo de Souza Barreto <danillosbarreto@gmail.com>
2024-10-08 18:15:29 -0300

    Aula 02

---
+++ database/migrations/2024_10_08_185106_create_projects_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\User;
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->foreignIdFor(User::class, 'created_by')->constrained('users');
+            $table->datetime('ends_at');
+            $table->json('tech_stack');
+            $table->string('status')->default('open');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
